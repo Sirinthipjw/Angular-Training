a336bfb27e24a1b297a60ef320ca42c67bffce93
Sirinthipjw <phuchokdee.31@gmail.com>
2024-07-14 08:33:03 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
+import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule,FormControl, FormGroup, Validators } from '@angular/forms';
+import { RouterLink,RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, DatePipe, DecimalPipe, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myagpro';
+  title = 'Tool List';
+  // imageCoverUrl = '/image/image4.png'
+  major = 'Programmer'
+
+  items=[
+    "A","B","C","D","E"
+  ]
+
+  size = "L"
+
+  now = new Date()
+  number = 1560
+
+  name = new FormControl('')
+
+  profileForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required)
+  })
+
+  profileSubmit(){
+    console.log(this.profileForm.valid)
+    console.log(this.profileForm.value)
+  }
+
+  products = [
+    {id :1, name:'T-Shirt', size:'M', price:390, releaseDate: new Date()},
+    {id :2, name:'Shirt', size:'L', price:290, releaseDate: new Date()},
+    {id :3, name:'Polo Shirt', size:'XL', price:300.5, releaseDate: new Date()},
+    {id :4, name:'Derss', size:'M', price:490, releaseDate: new Date()},
+    {id :5, name:'Jacket', size:'XXL', price:5900, releaseDate: new Date()}
+  ]
 }
---
+++ src/app/page1/page1.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page1',
+  standalone: true,
+  imports: [],
+  templateUrl: './page1.component.html',
+  styleUrl: './page1.component.css'
+})
+export class Page1Component {
+  imageCoverUrl1 = '/image/image2.png'
+  onImageClick(){
+    alert("สวัสดีจ้าาาาาาาาา")
+  }
+  major = 'Programmer'
+
+}
